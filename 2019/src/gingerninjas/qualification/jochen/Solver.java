b4dddf1aa5e7162eb8b597f7a8c8867997c4387f
Jochen Jägers <jochen.jaegers@gmail.com>
2019-06-22 17:10:34 +0200

    added empty solver

---
+++ 2019/src/gingerninjas/qualification/jochen/Solver.java
@@ -0,0 +1,35 @@
+package gingerninjas.qualification.jochen;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.ThreadContext;
+
+import gingerninjas.BaseSolver;
+import gingerninjas.qualification.Input;
+import gingerninjas.qualification.Output;
+import gingerninjas.qualification.QualiSolver;
+
+public class Solver extends QualiSolver
+{
+	private static final int	AVAILABLE_TOPPINGS	= 2;
+	private Integer[]			validEdgeLengths;
+	private int					ratingRange;
+	private static final int	RANGE_STEP			= 1;
+
+
+	@Override
+	protected void solve()
+	{
+		
+	}
+
+	
+}
