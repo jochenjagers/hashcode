e5e4a394dc8dbecde6a72b18880cd0dcc1002662
Jochen Jägers <jochen.jaegers@gmail.com>
2019-06-22 19:13:12 +0200

    cache interestFactor

--- 2019/src/gingerninjas/qualification/QualiSolver.java
+++ 2019/src/gingerninjas/qualification/QualiSolver.java
@@ -2,12 +2,15 @@ package gingerninjas.qualification;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 import gingerninjas.BaseSolver;
 
 public abstract class QualiSolver extends BaseSolver<Input, Output>
 {
+	private HashMap<Integer, Integer> factorCache = new HashMap<Integer, Integer>();
+	
 	@Override
 	protected Input createInput(File path) throws IOException
 	{
@@ -29,14 +32,19 @@ public abstract class QualiSolver extends BaseSolver<Input, Output>
 		int common = 0;
 		int unique1 = 0;
 		int unique2 = 0;
-		for(Integer tag1 : first.getLast().getTags())
-		{
-			if(second.getFirst().getTags().contains(tag1))
-				common++;
-			else
-				unique1++;
+		
+		int hash = first.getLast().hashCode() + second.getFirst().hashCode();
+		if(!factorCache.containsKey(hash)) {
+			for(Integer tag1 : first.getLast().getTags())
+			{
+				if(second.getFirst().getTags().contains(tag1))
+					common++;
+				else
+					unique1++;
+			}
+			unique2 = second.getFirst().getTags().size() - common;
+			factorCache.put(hash, Math.min(common, Math.min(unique1, unique2)));
 		}
-		unique2 = second.getFirst().getTags().size() - common;
-		return Math.min(common, Math.min(unique1, unique2));
+		return factorCache.get(hash);
 	}
 }
