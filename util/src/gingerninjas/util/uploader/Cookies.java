2488e588346ce44b61dc590ccfccd132d463f2ee
Jochen Jägers <jochen.jaegers@gmail.com>
2020-02-12 20:14:10 +0100

    load cooies from Cookies.txt

--- util/src/gingerninjas/util/uploader/Cookies.java
+++ util/src/gingerninjas/util/uploader/Cookies.java
@@ -1,11 +1,17 @@
 package gingerninjas.util.uploader;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.CookieHandler;
 import java.net.CookieManager;
 import java.net.HttpCookie;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
@@ -30,7 +36,8 @@ public class Cookies
 
 			cookieManager = new CookieManager();
 			CookieHandler.setDefault(cookieManager);
-
+			loadCookies();
+			/*
 			String key, value, domain;
 			for(int i = 1; i < 100; i++)
 			{
@@ -47,6 +54,7 @@ public class Cookies
 					break;
 				}
 			}
+			*/
 		}
 		catch(Exception e)
 		{
@@ -60,6 +68,52 @@ public class Cookies
 		cookie.setDomain(domain);
 		cookie.setPath("/");
 		cookieManager.getCookieStore().add(new URI("https://" + domain + "/"), cookie);
+	}
+	
+	private static void loadCookies() {
+		FileReader fr = null;
+		BufferedReader br = null;
+		try
+		{
+			fr = new FileReader("../cookies.txt");
+			br = new BufferedReader(fr);
+			
+			String line = null;
+			while((line = br.readLine()) != null) {
+				logger.debug("endpoint: " + line);
+				ArrayList<String> splittedLine = new ArrayList<>(Arrays.asList(line.split("\t")));
+			}
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally
+		{
+			if(br != null)
+			{
+				try
+				{
+					br.close();
+				}
+				catch(IOException e)
+				{
+					// ignore
+				}
+			}
+			if(fr != null)
+			{
+				try
+				{
+					fr.close();
+				}
+				catch(IOException e)
+				{
+					// ignore
+				}
+			}
+		}
 	}
 	
 	public static void main(String[] args)
