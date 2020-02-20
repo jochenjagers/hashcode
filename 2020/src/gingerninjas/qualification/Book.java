01e27dfdc5f2cb3e1551c841a39249684feab4d8
ultimate <ultimatej@gmx.net>
2020-02-20 19:01:46 +0100

    Added Book & Library

---
+++ 2020/src/gingerninjas/qualification/Book.java
@@ -0,0 +1,40 @@
+package gingerninjas.qualification;
+
+public class Book
+{
+	private int		id;
+	private int		score;
+	private boolean	scanned;
+
+	public Book(int id, int score)
+	{
+		this.score = score;
+		this.scanned = false;
+	}
+	
+	public int getId()
+	{
+		return id;
+	}
+
+	public int getScore()
+	{
+		return score;
+	}
+
+	public boolean isScanned()
+	{
+		return scanned;
+	}
+
+	public void setScanned(boolean scanned)
+	{
+		this.scanned = scanned;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Book [id=" + id + ", score=" + score + "]";
+	}
+}
---
+++ 2020/src/gingerninjas/qualification/Library.java
@@ -0,0 +1,53 @@
+package gingerninjas.qualification;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library
+{
+	private int			id;
+	private int			signupTime;
+	private int			booksPerDay;
+	private List<Book>	books;
+	private List<Book>	scannedBooks;
+	
+	public Library(int id, int signupTime, int booksPerDay)
+	{
+		super();
+		this.id = id;
+		this.signupTime = signupTime;
+		this.booksPerDay = booksPerDay;
+		this.books = new ArrayList<Book>();
+	}
+
+	public int getId()
+	{
+		return id;
+	}
+
+	public int getSignupTime()
+	{
+		return signupTime;
+	}
+
+	public int getBooksPerDay()
+	{
+		return booksPerDay;
+	}
+
+	public List<Book> getBooks()
+	{
+		return books;
+	}
+
+	public boolean addBook(Book book)
+	{
+		return this.books.add(book);
+	}
+
+	public boolean scanBook(Book book)
+	{
+		book.setScanned(true);
+		return this.scannedBooks.add(book);
+	}
+}
