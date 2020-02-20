465589cf0070abc201b9be312179083cfa855ebc
Jochen Jägers <jochen.jaegers@gmail.com>
2020-02-20 23:12:28 +0100

    added solver

--- 2020/src/gingerninjas/qualification/jochen/Solver.java
+++ 2020/src/gingerninjas/qualification/jochen/Solver.java
@@ -1,110 +1,147 @@
 package gingerninjas.qualification.jochen;
 
-import java.util.Date;
-import java.util.LinkedList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.ListIterator;
 
-import gingerninjas.qualification.Photo;
+import gingerninjas.qualification.Book;
+import gingerninjas.qualification.Library;
 import gingerninjas.qualification.QualiSolver;
-import gingerninjas.qualification.Slide;
 
-public class Solver extends QualiSolver
-{
-
+public class Solver extends QualiSolver {
+	public static final double TARGET_SCORE = 0.8;
+	public static final int MAX_TRIES = 100;
 
 	@Override
-	protected void solve()
-	{
-		//while(true) {
-			input.reset();
-			output.reset();
-		
-		// create startconditions
-		LinkedList<LinkedList<Slide>> chains = new LinkedList<>();
-		Photo v = null;
-		for(Photo p : input.getPhotos()) {
-			if(p.isHorizontal()) {
-				if(p.getTags().size() <= 1) {
-					continue;
-				}
-				LinkedList<Slide> l = new LinkedList<>();
-				l.add(new Slide(p));
-				chains.add(l);
-			} else {
-				if(v == null) {
-					v = p;
-				} else {
-					LinkedList<Slide> l = new LinkedList<>();
-					l.add(new Slide(p,v));
-					chains.add(l);
-					v = null;
-				}
-			}
-		}
-		long d = System.currentTimeMillis();
-		while(chains.size() > 1) {
-			
-			if((System.currentTimeMillis() - d) > 5000) {
-				logger.info(input.getName() + " chaincount: " + chains.size());
-				d = System.currentTimeMillis();
-			}
+	protected void solve() {
+		input.reset();
+		output.reset();
 
-			LinkedList<Slide> s = chains.getFirst();
-			//LinkedList<Slide> s = chains.get((int)(Math.random()*(chains.size()-1)));
-			
-			LinkedList<Slide> bestChain = null;
-			boolean front = true;
-			int points = 0;
-			int count = 0;
-			for(LinkedList<Slide> chain : chains) {
-			//for(int i = 0; i < 100; ++i) {
-				//LinkedList<Slide> chain = chains.get((int)(Math.random()*(chains.size()-1)));
-				//if(bestChain != null && count > 100) {
-				//	break;
-				//}
-				if(s != chain /*&& (chain.getFirst().getTags().size()/2 > ponits || chain.getLast().getTags().size()/2 > ponits)*/) {
-					int p1 = calcInterestFactor(s, chain);
-					int p2 = calcInterestFactor(chain, s);
-					if(p1 >= p2 && p1 > points) {
-						// more points
-						bestChain = chain;
-						front = true;
-						points = p1;
-						/*
-						if(s.getLast().getMaxScore() <= ponits && chain.getFirst().getMaxScore() <= ponits ) {
-							break;
-						}
-						*/
-					} else if(p2 > points) {
-						bestChain = chain;
-						front = false;
-						points = p2;
-						/*
-						if(chain.getLast().getMaxScore() <= ponits && s.getFirst().getMaxScore() <= ponits ) {
-							break;
-						}
-						*/
-					}
-				}
-			}
-			if(bestChain != null ) {
-				if(front == true) {
-					// more points
-					s.addAll(bestChain);
-					chains.remove(bestChain);
-				} else {
-					bestChain.addAll(s);
-					chains.remove(s);
-				}
-			} else {
-				s.addAll(chains.getLast());
-				chains.remove(chains.getLast());
-			}
-		}
-		output.addSlides(chains.getFirst());
-		output.calcScore();
-		logger.info("Points: " + output.getScore());
-		//}
+		quickAndDirty();
 	}
 
-	
+	public void quickAndDirty() {
+		//int remaining = input.getDaysForScanning();
+
+		List<Library> libs = input.getLibraries();
+		List<Book> books;
+
+		for (Library l : libs) {
+			l.sortBooksByScore();
+		}
+		boolean libAdded = true;
+		int days = 0;
+		while (days <= input.getDaysForScanning() && libs.size() > 0 && libAdded == true) {
+			libAdded = false;
+			logger.info("Days " + days);
+			final int remaining = input.getDaysForScanning() - days;
+			
+			ListIterator<Library> iter = libs.listIterator();
+			logger.info("Libs: " + libs.size());
+			while(iter.hasNext()){
+			    if(remaining - iter.next().getSignupTime() <= 0){
+			        iter.remove();
+			    }
+			}
+			if(libs.size() == 0) {
+				break;
+			}
+			logger.info("Libs: " + libs.size());
+			libs.sort(new Comparator<Library>() {
+				@Override
+				public int compare(Library o1, Library o2) {
+					double delta = ((double)o2.getPossibleScore(remaining) / (o2.getSignupTime())) - ((double)o1.getPossibleScore(remaining) / (o1.getSignupTime()));
+					if (delta > 0)
+						return 1;
+					else if (delta < 0)
+						return -1;
+					else
+						return 0;
+				}
+			});
+			int i = 0;
+			Library l = libs.get(0);
+			/*while(l.getSignupTime() > 210) {
+				i++;
+				l = libs.get(i);
+			}*/
+			
+			//
+			
+			
+			long capacity = l.getBooksPerDay() * ((long) remaining - l.getSignupTime());
+			int scanned = 0;
+			/* if(capacity > l.getBooks().size()) {
+				logger.info("Skip Library because of too few books " + l.toString());
+				libs.remove(0);
+				continue;
+			} */
+			// logger.info("processing " + l + ": remaining=" + remaining + ", capacity=" +
+			// capacity);
+			
+			if (output.addLibrary(l)) {
+				logger.info("Add library " + l.getPossibleScore(remaining)+ " " + l.toString());
+				books = l.getBooks();
+
+				books.sort(new Comparator<Book>() {
+					@Override
+					public int compare(Book o1, Book o2) {
+						return o2.getScore() - o1.getScore();
+					}
+				});
+
+				for (Book b : books) {
+					if (!b.isScanned() && scanned < capacity) {
+						l.scanBook(b);
+						scanned++;
+					}
+				}
+				if (scanned == 0) {
+					output.removeLibrary(l);
+				} else {
+					logger.info("Add library " + l.getPossibleScore(remaining)+ " " + l.toString());
+					libAdded = true;
+					days += l.getSignupTime();
+				}
+			}
+			libs.remove(0);
+		}
+		/*
+		days = 0;
+		for(int i = 0; i < output.getLibraries().size() -1; ++i) {
+			Library l1 = output.getLibraries().get(i);
+			Library l2 = output.getLibraries().get(i+1);
+			if(l1.getRemaining() > 0) {
+			for(Book b : l1.getScannedBooks()) {
+				b.setScanned(false);
+			}
+			l1.getScannedBooks().clear();
+			for(Book b : l2.getScannedBooks()) {
+				b.setScanned(false);
+			}
+			l2.getScannedBooks().clear();
+			days += l2.getSignupTime();
+			int scanned = 0;
+			long capacity = l2.getBooksPerDay() * ((long) (input.getDaysForScanning()-days)- l2.getSignupTime());
+			for (Book b : l2.getBooks()) {
+				if (!b.isScanned() && scanned < capacity) {
+					l2.scanBook(b);
+					scanned++;
+				}
+			}
+			days += l1.getSignupTime();
+			scanned = 0;
+			capacity = l1.getBooksPerDay() * ((long) (input.getDaysForScanning()-days)- l1.getSignupTime());
+			for (Book b : l1.getBooks()) {
+				if (!b.isScanned() && scanned < capacity) {
+					l1.scanBook(b);
+					scanned++;
+				}
+			}
+			output.getLibraries().set(i+1, l1);
+			output.getLibraries().set(i, l2);
+			}
+		}
+		*/
+	}
 }
