f40f886a93e7498f13e36065f65bff6cca4d168f
ultimate <ultimatej@gmx.net>
2020-02-20 19:23:00 +0100

    solver

--- 2020/src/gingerninjas/qualification/QualiSolver.java
+++ 2020/src/gingerninjas/qualification/QualiSolver.java
@@ -2,14 +2,12 @@ package gingerninjas.qualification;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.LinkedList;
 
 import gingerninjas.BaseSolver;
 
 public abstract class QualiSolver extends BaseSolver<Input, Output>
 {
-	private HashMap<Integer, Integer> factorCache = new HashMap<Integer, Integer>();
+//	private HashMap<Integer, Integer> factorCache = new HashMap<Integer, Integer>();
 	
 	@Override
 	protected Input createInput(File path) throws IOException
@@ -27,25 +25,25 @@ public abstract class QualiSolver extends BaseSolver<Input, Output>
 		return o;
 	}
 	
-	public int calcInterestFactor(LinkedList<Slide> first, LinkedList<Slide> second)
-	{
-		int common = 0;
-		int unique1 = 0;
-		int unique2 = 0;
-		
-		//int hash = first.getLast().hashCode() + second.getFirst().hashCode();
-		//if(!factorCache.containsKey(hash)) {
-			for(Integer tag1 : first.getLast().getTags())
-			{
-				if(second.getFirst().getTags().contains(tag1))
-					common++;
-				else
-					unique1++;
-			}
-			unique2 = second.getFirst().getTags().size() - common;
-			//factorCache.put(hash, //Math.min(common, Math.min(unique1, unique2)));
-		//}
-		//return factorCache.get(hash);
-		return Math.min(common, Math.min(unique1, unique2));
-	}
+//	public int calcInterestFactor(LinkedList<Slide> first, LinkedList<Slide> second)
+//	{
+//		int common = 0;
+//		int unique1 = 0;
+//		int unique2 = 0;
+//		
+//		//int hash = first.getLast().hashCode() + second.getFirst().hashCode();
+//		//if(!factorCache.containsKey(hash)) {
+//			for(Integer tag1 : first.getLast().getTags())
+//			{
+//				if(second.getFirst().getTags().contains(tag1))
+//					common++;
+//				else
+//					unique1++;
+//			}
+//			unique2 = second.getFirst().getTags().size() - common;
+//			//factorCache.put(hash, //Math.min(common, Math.min(unique1, unique2)));
+//		//}
+//		//return factorCache.get(hash);
+//		return Math.min(common, Math.min(unique1, unique2));
+//	}
 }
