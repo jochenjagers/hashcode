b617cc780deab403ce580277fd78d74a1f96e5fa
Jochen Jägers <jochen.jaegers@gmail.com>
2020-02-20 18:29:57 +0100

    added first version of solver

---
+++ 2020/src/gingerninjas/practice/jochen/Solver.java
@@ -0,0 +1,26 @@
+package gingerninjas.practice.jochen;
+
+import java.util.Date;
+import java.util.LinkedList;
+
+import gingerninjas.practice.PractiSolver;
+
+public class Solver extends PractiSolver
+{
+
+
+	@Override
+	protected void solve()
+	{
+		
+		int result = 0;
+		int index = 0;
+	
+		this.output.getTypes().add(0);
+		
+		this.updateScore();
+		
+	}
+
+	
+}
